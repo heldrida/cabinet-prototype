3250aaf01fe9ab0af6c4b5f91cc37b594d0358ba
Helder <helder@lamoulade.com>
2016-07-20 15:33:52 +0100

    refactored a bit, moved the cabinet module contained on its own file

---
+++ src/js/CabinetModule.js
@@ -0,0 +1,98 @@
+function CabinetModule (params) {
+
+	// config
+	this.debug = params.debug;
+
+	// properties
+	this.module = params.el;
+	this.layers = this.module.querySelectorAll('img');
+	this.bounds,
+	this.x,
+	this.y,
+	this.w,
+	this.h,
+	this.cx,
+	this.cy,
+	this.ix,
+	this.iy,
+	this.ax,
+	this.ay = null;
+
+	// methods
+	this.onAnimationFrame = function () {
+
+		// todo: cancel if mouse is not hover the module
+
+	    for (var i = 0; i < this.layers.length; i++) {
+	    	this.setStyle(this.layers[i], this.x, this.y);
+	    }
+
+	    // callback
+		requestAnimationFrame(this.onAnimationFrame.bind(this));
+
+	}
+
+	this.setStyle = function (element, x, y) {
+
+		var friction = element.getAttribute('data-friction');
+
+		// cast
+		friction = parseFloat(friction);
+
+		if (this.debug) {
+			console.log('friction', friction);
+		}
+
+	}
+
+	this.onMouseMove = function (event) {
+
+		this.bounds = this.module.getBoundingClientRect();
+
+		this.calcPositions.call(this, event);
+
+	}
+
+	this.calcPositions = function () {
+
+		// box module origin (top/left) positions
+		this.x = this.bounds.left;
+		this.y = this.bounds.top;
+
+		// box module size
+		this.w = this.bounds.width;
+		this.h = this.bounds.height;
+
+		// box module center
+		this.cx = this.w / 2;
+		this.cy = this.h / 2;
+
+		// offset from center
+		this.ix = (event.clientX - this.x - this.cx) / this.cx;
+		this.iy = (event.clientY - this.y - this.cy) / this.cy;
+
+		// amount from origin (top/left)
+		this.ax = event.clientX / this.w;
+		this.ay = event.clientY / this.h;
+
+		if (this.debug) {
+			console.log('w: ' + this.w + ', h: ' + this.h + ', x: ' + this.x + ', y: ' + this.y + ', cx: ' + this.cx + ', this.cy: ' + this.cy + ', ix: ' + this.ix + ', iy: ' + this.iy);
+		}
+
+	}
+
+	this.setEventListeners = function () {
+
+		window.addEventListener('mousemove', this.onMouseMove.bind(this));
+
+	}
+
+	// start event listeners
+	this.setEventListeners();
+
+	// initialse animation
+	requestAnimationFrame(this.onAnimationFrame.bind(this));
+
+}
+
+module.exports = CabinetModule;
